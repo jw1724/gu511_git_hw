a9c17b34f057a23dbdc5f13b2d6d60d4f19c342a
Jingze Wang <jerry@JingzedeMBP.fios-router.home>
2018-11-09 23:29:45 -0500

    dspipeline: adding usage instructions

--- dspipeline.py
+++ dspipeline.py
@@ -578,10 +578,7 @@ def adult_data_demo():
     # to bootstrapped sub-samples)
     p_best.fit(xtrain, ytrain)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/minortweak
     # get ccr values on test data
     df_ccr = get_ccr_df(p_best, xtest, ytest)
     ccr_fig = make_ccr_plot(df_ccr)
